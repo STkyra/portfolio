f1df4dfe1dc62e0325ec01ea2eb095414b99d322
STkyra <528977@student.fontys.nl>
2024-06-17 17:31:57 +0200

    auto formatted alles

--- carousel.js
+++ carousel.js
@@ -15,14 +15,14 @@ function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("containers");
   var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
+  if (n > slides.length) { slidePosition = 1 }
+  if (n < 1) { slidePosition = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
+    circles[i].className = circles[i].className.replace(" enable", "");
   }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+  slides[slidePosition - 1].style.display = "block";
+  circles[slidePosition - 1].className += " enable";
 } 
