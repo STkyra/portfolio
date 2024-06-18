6445c4aa3127f79063e7782488aaea6c5e7d6f0d
STkyra <528977@student.fontys.nl>
2024-06-18 16:38:15 +0200

    Begonnen met alles naar engles zetten

--- Js/carousel.js
+++ Js/carousel.js
@@ -1,28 +1,28 @@
 var slidePosition = 1;
 SlideShow(slidePosition);
 
-// forward/Back controls
+// Vooruit/terug controls
 function plusSlides(n) {
   SlideShow(slidePosition += n);
 }
 
-//  images controls
+// Afbeeldingen controls
 function currentSlide(n) {
   SlideShow(slidePosition = n);
 }
 
 function SlideShow(n) {
   var i;
-  var slides = document.getElementsByClassName("containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) { slidePosition = 1 }
-  if (n < 1) { slidePosition = slides.length }
+  var slides = document.getElementsByClassName("containers"); // Haal alle elementen met class "containers" op
+  var circles = document.getElementsByClassName("dots"); // Haal alle elementen met class "dots" op
+  if (n > slides.length) { slidePosition = 1 } // Als de huidige positie groter is dan het aantal slides, ga naar de eerste slide
+  if (n < 1) { slidePosition = slides.length } // Als de huidige positie kleiner is dan 1, ga naar de laatste slide
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none"; // Verberg alle slides
   }
   for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
+    circles[i].className = circles[i].className.replace(" enable", ""); // Verwijder de "enable" class van alle stippen
   }
-  slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
-} 
+  slides[slidePosition - 1].style.display = "block"; // Toon de huidige slide
+  circles[slidePosition - 1].className += " enable"; // Voeg de "enable" class toe aan de huidige stip
+}
--- Js/navbar.js
+++ Js/navbar.js
@@ -1,12 +1,14 @@
+// Wanneer de gebruiker scrolt, voer de myFunction functie uit
 window.onscroll = function () { myFunction() };
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+var navbar = document.getElementById("navbar"); // Haal het navbar element op
+var sticky = navbar.offsetTop; // Bewaar de offset positie van de navbar
 
 function myFunction() {
+  // Controleer of de pagina verticaal is verschoven tot voorbij de offset positie van de navbar
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky"); // Voeg de "sticky" class toe aan de navbar
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky"); // Verwijder de "sticky" class van de navbar
   }
 }
